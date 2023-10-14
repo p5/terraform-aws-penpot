78f59c20b4593c29eaa842ed35d6cf672970c104
Robert Sturla <robertsturla@outlook.com>
2023-10-14 22:05:58 +0100

    Initial rds module

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
+variable "name" {
+  type        = string
+  description = "The name of the PenPot instance"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID to use"
+}
+
+variable "database_subnet_ids" {
+  type        = list(string)
+  description = "The subnet IDs to use for the database"
+}
+
+variable "database_instance_class" {
+  type        = string
+  description = "The instance class to use for the database"
+  default     = null
+}
+
+variable "database_max_allocated_storage" {
+  type        = number
+  description = "The maximum allocated storage in gigabytes for the database"
+  default     = null
+}
+
+variable "database_engine_version" {
+  type        = string
+  description = "The engine version to use for the database"
+  default     = null
+}
+
 # ECS Fargate Cluster
 module "ecs_cluster" {
   source = "./modules/ecs-cluster"
@@ -7,6 +40,14 @@ module "ecs_cluster" {
 # RDS Database
 module "rds" {
   source = "./modules/rds"
+
+  name       = var.name
+  vpc_id     = var.vpc_id
+  subnet_ids = var.database_subnet_ids
+
+  instance_class        = var.database_instance_class
+  max_allocated_storage = var.database_max_allocated_storage
+  engine_version        = var.database_engine_version
 }
 
 
