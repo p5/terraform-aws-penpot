855daf20d3885d9e344685e9e8dd7f39d5159f00
Robert Sturla <robertsturla@outlook.com>
2023-10-14 20:55:37 +0100

    Plan module structure

---
+++ main.tf
@@ -0,0 +1,40 @@
+# ECS Fargate Cluster
+# module "ecs_cluster" {
+#   source = "./modules/ecs-cluster"
+# }
+
+
+# RDS Database
+# module "rds" {
+#   source = "./modules/rds"
+# }
+
+
+# Elasticache Redis
+# module "redis" {
+#   source = "./modules/redis"
+# }
+
+
+# S3 Bucket
+# module "s3" {
+#   source = "./modules/s3"
+# }
+
+
+# SES
+# module "ses" {
+#   source = "./modules/ses"
+# }
+
+
+# Application Load Balancer
+# module "alb" {
+#   source = "./modules/alb"
+# }
+
+
+# ECS Fargate Service (Frontend, Backend, Minio. Exporter)
+# module "ecs_service" {
+#   source = "./modules/ecs-service"
+# }
