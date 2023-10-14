939d5b6016cc141f3c7a0530c45e1dc14929248b
Robert Sturla <robertsturla@outlook.com>
2023-10-14 21:02:35 +0100

    Enable all modules

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 # ECS Fargate Cluster
-# module "ecs_cluster" {
-#   source = "./modules/ecs-cluster"
-# }
+module "ecs_cluster" {
+  source = "./modules/ecs-cluster"
+}
 
 
 # RDS Database
-# module "rds" {
-#   source = "./modules/rds"
-# }
+module "rds" {
+  source = "./modules/rds"
+}
 
 
 # Elasticache Redis
-# module "redis" {
-#   source = "./modules/redis"
-# }
+module "redis" {
+  source = "./modules/redis"
+}
 
 
 # S3 Bucket
-# module "s3" {
-#   source = "./modules/s3"
-# }
+module "s3" {
+  source = "./modules/s3"
+}
 
 
 # Application Load Balancer
-# module "alb" {
-#   source = "./modules/alb"
-# }
+module "alb" {
+  source = "./modules/alb"
+}
 
 
 # ECS Fargate Service (Frontend, Backend, Minio. Exporter)
-# module "ecs_service" {
-#   source = "./modules/ecs-service"
-# }
+module "ecs_service" {
+  source = "./modules/ecs-service"
+}
