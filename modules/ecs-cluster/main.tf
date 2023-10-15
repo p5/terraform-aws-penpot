28aad84e7d2e85bf4ce883784fefbd64452e0c4a
Robert Sturla <robertsturla@outlook.com>
2023-10-15 10:21:01 +0100

    Move variables to variables.tf

--- modules/ecs-cluster/main.tf
+++ modules/ecs-cluster/main.tf
@@ -1,14 +1,3 @@
-variable "name" {
-  type        = string
-  description = "The name of the ECS cluster"
-}
-
-variable "container_insights_enabled" {
-  type        = bool
-  description = "Whether to enable container insights"
-  default     = true
-}
-
 resource "aws_ecs_cluster" "this" {
   name = var.name
 
